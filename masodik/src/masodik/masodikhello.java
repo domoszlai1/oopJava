5ed338a5ddea3092f4eea4dd9f52e637594b8328
domoszlai1 <dm@outlook.hu>
2018-02-20 20:10:24 +0100

    tesztelés megint

---
+++ masodik/src/masodik/masodikhello.java
@@ -0,0 +1,11 @@
+package masodik;
+
+public class masodikhello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world github");
+		
+	}
+
+}
